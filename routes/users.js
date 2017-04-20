e84eb70ed4d7ba6c55225a1d7c1c471271604b08
Tarunendra Dwivedi (Accenture) <v-tadwiv@microsoft.com>
2017-04-19 22:59:39 -0700

    Initial

---
+++ routes/users.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+var fs = require('fs');
+var csv = require('fast-csv');
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  var queryParam1 = req.query.technology;
+ 
+  var skillDataArr = new Array();
+ // var stream = fs.createReadStream("Skills_Tarun.csv");
+ var stream = fs.createReadStream('RosterEmployees.csv');
+  csv
+  .fromStream(stream, {headers : true})
+  .on("data", function(data){
+      skillDataArr.push(data);
+  })
+  .on("end", function(){
+      //console.log(skillDataArr.length);
+      module.exports.saveData(skillDataArr);
+      console.log("done");
+  });
+
+  console.log('Before send..');
+
+  module.exports.saveData = function(data, callback) {
+   // console.log(data)
+    console.log('inside callback function');
+    
+    var items = skillDataArr.filter(function(item){
+      console.log('Printing items skills data --->'+item.StandardJob)
+      var flag = item.StandardJob.includes(queryParam1);
+      console.log('Match status --->'+flag)
+      return flag;
+    });
+
+    res.send(items);
+}
+
+console.log('AT THE END---'+skillDataArr.length);
+
+});
+
+module.exports = router;
